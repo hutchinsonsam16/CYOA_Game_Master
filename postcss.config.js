3037decca5f592a86cd7a61a5bb71168d721e55b
hutchinsonsam16 <hutchinsonsam.16@gmail.com>
2025-09-08 14:27:05 +0530

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,12 @@
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
+  content: [
+    "./index.html",
+    "./index.tsx",
+    "./index.css",
+    "./components/**/*.{js,ts,jsx,tsx}", // if you have any
+  ],
+  theme: {
+    extend: {},
   },
+  plugins: [],
 };
