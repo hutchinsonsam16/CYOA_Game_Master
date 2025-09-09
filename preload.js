449ec33428d529107d3ac6f2e29510a6c8287aa0
hutchinsonsam16 <hutchinsonsam.16@gmail.com>
2025-09-09 19:48:13 +0530

    Update preload.js

--- preload.js
+++ preload.js
@@ -2,6 +2,11 @@
 // It has the same sandbox as a Chrome extension.
 // This file is used to securely expose a limited set of Node.js APIs
 // to the renderer process, but for this application, no APIs are needed.
-const { contextBridge } = require('electron');
+const { contextBridge, app } = require('electron');
+const path = require('path');
 
-contextBridge.exposeInMainWorld('electronAPI', {});
+const modelsPath = path.join(app.getAppPath(), 'models');
+
+contextBridge.exposeInMainWorld('electronAPI', {
+    modelsPath: modelsPath,
+});
