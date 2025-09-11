5080b63426dbed4b87410b82e289c9659a97d477
hutchinsonsam16 <hutchinsonsam.16@gmail.com>
2025-09-11 14:38:03 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,18 @@
-import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig(({ mode }) => {
-    +  const env = loadEnv(mode, '.', '');
-    +  return {
-        +    define: {
-        +      'import.meta.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-        +    },
-    +    resolve: {
-        +      alias: {
-            +        '@': path.resolve(__dirname, '.'),
-                +      }
-        +    }
-    +  };
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    plugins: [react()],
+    define: {
+      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      }
+    }
+  };
 });
